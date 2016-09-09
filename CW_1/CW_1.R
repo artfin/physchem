33bf127afbe9e7f998be196682927731ae14ab98
artfin <artfin@mail.ru>
2016-09-09 23:49:28 +0300

    interim

--- CW_1/CW_1.R
+++ CW_1/CW_1.R
@@ -1,3 +1,5 @@
+require(MESS)
+
 a <- 3.592
 b <- 0.04267
 
@@ -5,8 +7,8 @@ generate_arrays <- function(Temp) {
   x <- array()
   y <- array()
   
-  x[1] <- b + 0.0001
-  for (i in 2 : 10000) {
+  x[1] <- b + 0.00001
+  for (i in 2 : 100000) {
     x <- c(x, b + 0.001 * i)
   }
   y <- (0.082 * Temp) / (x - b) - (a / (x * x))
@@ -18,100 +20,89 @@ generate_arrays <- function(Temp) {
 }
 
 q <- list()
-for (i in 1 : 1) {
+for (i in 1 : 5) {
   q[[i]] <- generate_arrays(245 + i * 15)
+  cat(i, "\n")
 }
 
 plot(q[[1]][[1]], q[[1]][[2]], main = "vdW", xlab = "V", ylab = "p", type = "l", xlim = c(0, 3), ylim = c(0, 100))
-for (i in 1 : 1) {
+for (i in 1 : 5) {
   points(q[[i]][[1]], q[[i]][[2]], type = "l", col= i, lwd = 2)
 }
 
-spinodal <- array()
-spinodal <- a * (q[[1]][[1]] - 2 * b) / (q[[1]][[1]]^3)
-points(q[[1]][[1]], spinodal, type = "l", col = 1, lwd = 2)
+# spinodal <- array()
+# spinodal <- a * (q[[1]][[1]] - 2 * b) / (q[[1]][[1]]^3)
+# points(q[[1]][[1]], spinodal, type = "l", col = 1, lwd = 2)
 
-maxima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==-2) + 1]
-minima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==+2) + 1]
+# maxima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==-2) + 1]
+# minima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==+2) + 1]
 
-abline(h = maxima, col = "red", lwd = 2) 
-abline(h = minima, col = "red", lwd = 2) 
+for (counter in 1 : 5) {
+  maxima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==-2) + 1]
+  minima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==+2) + 1]
 
-med <- (maxima + minima) / 2
-abline(h = med, col = "green", lwd = 2)
+  for (s in 1 : 1000) {
+  # current height
+  h <- minima + s * (maxima - minima) / 1000
 
-err <- 0.3
-res <- list()
-res[[1]] <- c(0, 0)
-for (i in 1 : length(q[[1]][[2]])) {
-  if (abs(q[[1]][[2]][i] - med) < err) {
-    res[[length(res) + 1]] <- c(q[[1]][[1]][i], q[[1]][[2]][i])
-  }
-}
-res[[1]] <- NULL
-
-for (i in 1 : length(res)) {
-  if (res[[i]][1] - res[[i]])
-}
-
-for (i in 1 : length(res)) {
-  points(res[[i]][1], res[[i]][2], col = "blue", lwd = 2)
-}
-
-
-require(MESS)
-auc(q[[1]][[1]], q[[1]][[2]], type = 'spline')
-
-######################
-
-x <- seq(-3,3,0.01)
-y1 <- dnorm(x,0,1)
-y2 <- 0.5*dnorm(x,0,1)
-plot(x,y1,type="l",bty="L",xlab="X",ylab="dnorm(X)")
-points(x,y2,type="l",col="red")
-polygon(c(x,rev(x)),c(y2,rev(y1)),col="skyblue")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-find_extremum <- function(x, y) {
-  min <- array(0, dim = 1)
-  max <- array(0, dim = 1)
-  
-  for (i in 2 : (length(x) - 1)) {
-    if (y[i] > y[i - 1] && y[i] < y[i + 1]) {
-      max <- c(max, x[i])
-    }
-    if (y[i] < y[i - 1] && y[i] < y[i + 1]) {
-      min <- c(min, x[i])
-    }
-  }
-  
-  min <- min[-1]
-  max <- max[-1]
-  
+  err <- 0.3
   res <- list()
-  res[[1]] <- min
-  res[[2]] <- max
-  return(res)
+  res[[1]] <- c(0, 0)
+  for (i in 1 : length(q[[1]][[2]])) {
+    if (abs(q[[1]][[2]][i] - h) < err) {
+      res[[length(res) + 1]] <- c(q[[1]][[1]][i], q[[1]][[2]][i])
+    }
+  }
+  res[[1]] <- NULL
+
+  # deleting all near by dots
+  for (i in 1 : (length(res) - 1)) {
+    if (abs(res[[i]][1] - res[[i + 1]][1]) < 0.01) {
+      res[[i]] <- c(0,0)
+    }
+  }
+  
+  # isolating intersection points
+  intersection <- list()
+  counter <- 1
+  for (i in 1 : length(res)) {
+    if (res[[i]][1] != 0 && res[[i]][2] != 0) {
+      intersection[[counter]] <- c(res[[i]][1], res[[i]][2])
+      counter <- counter + 1
+    }
+  }
+  
+  # finding out the number of intersection point on x-axis 
+  intersection_number <- array()
+  for (i in 1 : length(intersection)) {
+    intersection_number[i] <- which(intersection[[i]][1] == q[[1]][[1]])
+  }
+  
+  if (length(intersection_number) > 2) {
+    # trimming dots representing left and right humps
+    x_trimmed_left <- q[[1]][[1]][intersection_number[1] : intersection_number[2]]
+    y_trimmed_left <- q[[1]][[2]][intersection_number[1] : intersection_number[2]]
+    
+    x_trimmed_right <- q[[1]][[1]][intersection_number[2] : intersection_number[3]]
+    y_trimmed_right <- q[[1]][[2]][intersection_number[2] : intersection_number[3]]
+    
+    # finding humps' surfaces
+    s_under_left <- auc(x_trimmed_left, y_trimmed_left, type = 'spline')
+    s_under_right <- auc(x_trimmed_right, y_trimmed_right, type = 'spline')
+    
+    s_in_left <- q[[1]][[2]][intersection_number[1]] * (q[[1]][[1]][intersection_number[2]] - q[[1]][[1]][intersection_number[1]]) - s_under_left
+    s_in_right <- s_under_right - q[[1]][[2]][intersection_number[2]] * (q[[1]][[1]][intersection_number[3]] - 
+                                                                           q[[1]][[1]][intersection_number[2]])
+    
+    cat(s, ":  ", s_in_left - s_in_right, "\n")
+    if (abs(s_in_left - s_in_right) < 0.5) {
+      cat("Found it! \n")
+      break
+    } 
+  }
 }
-
-s <- find_extremum(q[[1]][[1]], q[[1]][[2]])
-
+  abline(h = maxima, col = counter, lwd = 2) 
+  abline(h = minima, col = counter, lwd = 2) 
+  abline(h = h, col = counter, lwd = 2)
+  cat("h: ", h, "\n")
+}
