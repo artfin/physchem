368cca76509f87ef943e7f3fccdc2a9f8fea4451
artfin <artfin@mail.ru>
2016-09-10 12:29:37 +0300

    interim

--- CW_1/CW_1.R
+++ CW_1/CW_1.R
@@ -41,68 +41,77 @@ for (counter in 1 : 5) {
   maxima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==-2) + 1]
   minima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==+2) + 1]
 
-  for (s in 1 : 1000) {
-  # current height
-  h <- minima + s * (maxima - minima) / 1000
+  if (length(maxima) != 0 && length(minima) != 0) {
+    for (s in 1 : 1000) {
+      # current height
+      h <- minima + s * (maxima - minima) / 1000
+      
+      err <- 0.3
+      res <- list()
+      res[[1]] <- c(0, 0)
+      for (i in 1 : length(q[[counter]][[2]])) {
+        if (abs(q[[counter]][[2]][i] - h) < err) {
+          res[[length(res) + 1]] <- c(q[[counter]][[1]][i], q[[counter]][[2]][i])
+        }
+      }
+      res[[1]] <- NULL
+      
+      # deleting all near by dots
+      for (i in 1 : (length(res) - 1)) {
+        if (abs(res[[i]][1] - res[[i + 1]][1]) < 0.01) {
+          res[[i]] <- c(0,0)
+        }
+      }
+      
+      # isolating intersection points
+      intersection <- list()
+      k <- 1
+      for (i in 1 : length(res)) {
+        if (res[[i]][1] != 0 && res[[i]][2] != 0) {
+          intersection[[k]] <- c(res[[i]][1], res[[i]][2])
+          points(intersection[[k]][1], intersection[[k]][2], col = "blue", pch = 20)
+          k <- k + 1
+        }
+      }
 
-  err <- 0.3
-  res <- list()
-  res[[1]] <- c(0, 0)
-  for (i in 1 : length(q[[1]][[2]])) {
-    if (abs(q[[1]][[2]][i] - h) < err) {
-      res[[length(res) + 1]] <- c(q[[1]][[1]][i], q[[1]][[2]][i])
+      
+      # finding out the number of intersection point on x-axis 
+      intersection_number <- array()
+      for (i in 1 : length(intersection)) {
+        intersection_number[i] <- which(intersection[[i]][1] == q[[counter]][[1]])
+      }
+      
+      if (length(intersection_number) > 2) {
+        # trimming dots representing left and right humps
+        x_trimmed_left <- q[[counter]][[1]][intersection_number[1] : intersection_number[2]]
+        y_trimmed_left <- q[[counter]][[2]][intersection_number[1] : intersection_number[2]]
+        
+        x_trimmed_right <- q[[counter]][[1]][intersection_number[2] : intersection_number[3]]
+        y_trimmed_right <- q[[counter]][[2]][intersection_number[2] : intersection_number[3]]
+        
+        # finding humps' surfaces
+        s_under_left <- auc(x_trimmed_left, y_trimmed_left, type = 'spline')
+        s_under_right <- auc(x_trimmed_right, y_trimmed_right, type = 'spline')
+        
+        s_in_left <- q[[counter]][[2]][intersection_number[1]] * (q[[counter]][[1]][intersection_number[2]] - 
+                                                                    q[[counter]][[1]][intersection_number[1]]) - s_under_left
+        s_in_right <- s_under_right - q[[counter]][[2]][intersection_number[2]] * (q[[counter]][[1]][intersection_number[3]] - 
+                                                                                     q[[counter]][[1]][intersection_number[2]])
+        
+        cat(s, ":  ", s_in_left - s_in_right, "\n")
+        if (abs(s_in_left - s_in_right) < 0.5) {
+          cat("Found it! \n")
+          break
+        } 
+      }
     }
-  }
-  res[[1]] <- NULL
-
-  # deleting all near by dots
-  for (i in 1 : (length(res) - 1)) {
-    if (abs(res[[i]][1] - res[[i + 1]][1]) < 0.01) {
-      res[[i]] <- c(0,0)
-    }
-  }
-  
-  # isolating intersection points
-  intersection <- list()
-  counter <- 1
-  for (i in 1 : length(res)) {
-    if (res[[i]][1] != 0 && res[[i]][2] != 0) {
-      intersection[[counter]] <- c(res[[i]][1], res[[i]][2])
-      counter <- counter + 1
-    }
-  }
-  
-  # finding out the number of intersection point on x-axis 
-  intersection_number <- array()
-  for (i in 1 : length(intersection)) {
-    intersection_number[i] <- which(intersection[[i]][1] == q[[1]][[1]])
-  }
-  
-  if (length(intersection_number) > 2) {
-    # trimming dots representing left and right humps
-    x_trimmed_left <- q[[1]][[1]][intersection_number[1] : intersection_number[2]]
-    y_trimmed_left <- q[[1]][[2]][intersection_number[1] : intersection_number[2]]
-    
-    x_trimmed_right <- q[[1]][[1]][intersection_number[2] : intersection_number[3]]
-    y_trimmed_right <- q[[1]][[2]][intersection_number[2] : intersection_number[3]]
-    
-    # finding humps' surfaces
-    s_under_left <- auc(x_trimmed_left, y_trimmed_left, type = 'spline')
-    s_under_right <- auc(x_trimmed_right, y_trimmed_right, type = 'spline')
-    
-    s_in_left <- q[[1]][[2]][intersection_number[1]] * (q[[1]][[1]][intersection_number[2]] - q[[1]][[1]][intersection_number[1]]) - s_under_left
-    s_in_right <- s_under_right - q[[1]][[2]][intersection_number[2]] * (q[[1]][[1]][intersection_number[3]] - 
-                                                                           q[[1]][[1]][intersection_number[2]])
-    
-    cat(s, ":  ", s_in_left - s_in_right, "\n")
-    if (abs(s_in_left - s_in_right) < 0.5) {
-      cat("Found it! \n")
-      break
-    } 
+  } else {
+    cat("We are above critical temperature! \n")
   }
 }
-  abline(h = maxima, col = counter, lwd = 2) 
-  abline(h = minima, col = counter, lwd = 2) 
+
+#  abline(h = maxima, col = counter, lwd = 2) 
+#  abline(h = minima, col = counter, lwd = 2) 
   abline(h = h, col = counter, lwd = 2)
   cat("h: ", h, "\n")
 }
