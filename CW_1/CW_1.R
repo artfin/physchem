b210501dcaa0f5119e05fa2377a69b75a6698d98
Artfin <artfin@mail.ru>
2016-09-11 21:21:58 +0300

    interim

--- CW_1/CW_1.R
+++ CW_1/CW_1.R
@@ -8,8 +8,12 @@ generate_arrays <- function(Temp) {
   y <- array()
   
   x[1] <- b + 0.00001
-  for (i in 2 : 100000) {
-    x <- c(x, b + 0.001 * i)
+  for (i in 2 : 50000) {
+    x <- c(x, b + 0.00001 * i)
+  }
+  
+  for (i in 1 : 10000) {
+    x <- c(x, tail(x, 1) + 0.001 * i)
   }
   y <- (0.082 * Temp) / (x - b) - (a / (x * x))
   
@@ -20,13 +24,16 @@ generate_arrays <- function(Temp) {
 }
 
 q <- list()
-for (i in 1 : 5) {
-  q[[i]] <- generate_arrays(245 + i * 15)
-  cat(i, "\n")
+for (i in 1 : 15) {
+  q[[i]] <- generate_arrays(235 + i * 5)
+  cat("Generated: ", i, "\n")
 }
 
-plot(q[[1]][[1]], q[[1]][[2]], main = "vdW", xlab = "V", ylab = "p", type = "l", xlim = c(0, 3), ylim = c(0, 100))
-for (i in 1 : 5) {
+T_critical <- 
+
+# Plotting isothermes
+plot(q[[5]][[1]], q[[5]][[2]], main = "vdW", xlab = "V", type = "l", ylab = "p", xlim = c(0, 3), ylim = c(0, 100), lwd = 2)
+for (i in 6 : 10) {
   points(q[[i]][[1]], q[[i]][[2]], type = "l", col= i, lwd = 2)
 }
 
@@ -37,12 +44,15 @@ for (i in 1 : 5) {
 # maxima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==-2) + 1]
 # minima <- q[[1]][[2]][which(diff(sign(diff(q[[1]][[2]])))==+2) + 1]
 
-for (counter in 1 : 5) {
+binoidal_points <- list()
+binoidal_points_counter <- 1
+
+for (counter in 1 : 15) {
   maxima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==-2) + 1]
   minima <- q[[counter]][[2]][which(diff(sign(diff(q[[counter]][[2]])))==+2) + 1]
 
-  if (length(maxima) != 0 && length(minima) != 0) {
-    for (s in 1 : 1000) {
+  if (length(maxima) != 0 && length(minima) != 0 && minima > 0) {
+    for (s in 1 : 3000) {
       # current height
       h <- minima + s * (maxima - minima) / 1000
       
@@ -69,7 +79,7 @@ for (counter in 1 : 5) {
       for (i in 1 : length(res)) {
         if (res[[i]][1] != 0 && res[[i]][2] != 0) {
           intersection[[k]] <- c(res[[i]][1], res[[i]][2])
-          points(intersection[[k]][1], intersection[[k]][2], col = "blue", pch = 20)
+#          points(intersection[[k]][1], intersection[[k]][2], col = "blue", pch = 20)
           k <- k + 1
         }
       }
@@ -99,19 +109,45 @@ for (counter in 1 : 5) {
                                                                                      q[[counter]][[1]][intersection_number[2]])
         
         cat(s, ":  ", s_in_left - s_in_right, "\n")
-        if (abs(s_in_left - s_in_right) < 0.5) {
+        if (abs(s_in_left - s_in_right) < 0.05) {
           cat("Found it! \n")
+          abline(h = h, col = counter, lwd = 2)
+          cat("h: ", h, "\n")
+          cat("s1: ", s_in_left, "\n")
+          cat("s2: ", s_in_right, "\n")
+          
+          # filling binoidal-points-list
+          binoidal_points[[binoidal_points_counter]] <- c(q[[counter]][[1]][intersection_number[1]], 
+                                                          q[[counter]][[2]][intersection_number[1]])
+          binoidal_points[[binoidal_points_counter + 1]] <- c(q[[counter]][[1]][intersection_number[3]],
+                                                              q[[counter]][[2]][intersection_number[3]])
+          binoidal_points_counter <- binoidal_points_counter + 2
           break
         } 
       }
     }
   } else {
-    cat("We are above critical temperature! \n")
+    if (length(minima) != 0) {
+      cat("This is wrong isotherm! \n")
+    } else {
+      cat("We are above critical temperature! \n")
+    }
   }
 }
 
-#  abline(h = maxima, col = counter, lwd = 2) 
-#  abline(h = minima, col = counter, lwd = 2) 
-  abline(h = h, col = counter, lwd = 2)
-  cat("h: ", h, "\n")
+
+x <- array()
+y <- array()
+
+for (i in 1 : length(binoidal_points)) {
+  x <- c(x, binoidal_points[[i]][1])
+  y <- c(y, binoidal_points[[i]][2])
+  points(binoidal_points[[i]][1], binoidal_points[[i]][2], pch = 20, col = "blue")
 }
+x <- x[-1]
+y <- y[-1]
+
+func <- splinefun(x, y, method="natural",  ties = mean)
+x_for_binoidal <- seq(0, 1, by = 0.01)
+points(x_for_binoidal, func(x_for_binoidal), col = "green", type = "l", lwd = 2)
+points(x, y, col = "green", pch = 20)
